cefa66b7c39ef1996a54116aa4f489eea09f0755
김준하 <kimjoonha0426@gmail.com>
2025-12-10 17:36:05 +0900

    1210 / 17 / kjh / auth / define Users select Qwery for login

--- src/main/java/com/mycom/myapp/users/repository/UsersRepository.java
+++ src/main/java/com/mycom/myapp/users/repository/UsersRepository.java
@@ -2,6 +2,19 @@ package com.mycom.myapp.users.repository;
 
 import com.mycom.myapp.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<Users, Integer> {
+
+    @Query("""
+    select u
+    from Users u
+    join fetch u.usersRoles ur
+    join fetch ur.role
+    where u.email = :email and u.isDeleted = false
+    """)
+    Optional<Users> findByEmailForLogin(@Param("email") String email);
 }
